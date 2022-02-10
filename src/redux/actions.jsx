1feb42d8743123e5ab48b22c157547933ae3950b
Igor <glontyid@yandex.ru>
2022-02-10 15:29:31 +0400

    change actions

--- src/redux/actions.jsx
+++ src/redux/actions.jsx
@@ -2,18 +2,16 @@ import axios from "axios";
 import { setWeather } from "./reducer";
 
 export const getWeather = () => async (dispatch) => {
-  const getLocation = new Promise((resolve) => {
+  const getLocation = await new Promise((resolve) => {
     navigator.geolocation.getCurrentPosition(function (position) {
       resolve(position)
     });
   });
 
-  getLocation.then((res) => {
-    axios(`https://api.weatherapi.com/v1/forecast.json?key=eeb70c97c50a4ebfbbe145845220202&q=${res.coords.latitude},${res.coords.longitude}&days=10&lang=ru`)
-      .then(response => {
-        if (response.status === 200) {
-          dispatch(setWeather(response.data))
-        }
-      })
-  })
+  axios(`https://api.weatherapi.com/v1/forecast.json?key=eeb70c97c50a4ebfbbe145845220202&q=${getLocation.coords.latitude},${getLocation.coords.longitude}&days=10&lang=ru`)
+    .then(response => {
+      if (response.status === 200) {
+        dispatch(setWeather(response.data))
+      }
+    })
 };
